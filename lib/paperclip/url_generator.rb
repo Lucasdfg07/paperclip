b67a88447e5c5c7c146e4ca2c2a3c9fb461333c3
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2021-07-16 12:13:19 -0300

    Setting CGI escape method

--- lib/paperclip/url_generator.rb
+++ lib/paperclip/url_generator.rb
@@ -1,5 +1,27 @@
 require 'uri'
 require 'active_support/core_ext/module/delegation'
+module Paperclip
+  module UrlGenerator
+    class GCI
+      def escape(string)
+        encoding = string.encoding
+        string.b.gsub(/([^ a-zA-Z0-9_.\-~]+)/) do |m|
+          '%' + m.unpack('H2' * m.bytesize).join('%').upcase
+        end.tr(' ', '+').force_encoding(encoding)
+      end
+
+      # URL-decode a string with encoding(optional).
+      #   string = CGI.unescape("%27Stop%21%27+said+Fred")
+      #      # => "'Stop!' said Fred"
+      def unescape(string,encoding=@@accept_charset)
+        str=string.tr('+', ' ').b.gsub(/((?:%[0-9a-fA-F]{2})+)/) do |m|
+          [m.delete('%')].pack('H*')
+        end.force_encoding(encoding)
+        str.valid_encoding? ? str : str.force_encoding(string.encoding)
+      end
+    end
+  end
+end
 
 module Paperclip
   class UrlGenerator
